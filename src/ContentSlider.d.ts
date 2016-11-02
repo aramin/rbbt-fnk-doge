b2d2474cb728dbafb8a44afc2205d416dc3dad8d
Alexander Weiher <aweiher@users.noreply.github.com>
2016-11-02 10:55:02 +0100

    ts: update type description file

--- src/ContentSlider.d.ts
+++ src/ContentSlider.d.ts
@@ -1,46 +1,50 @@
+export = ContentSlider;
 
 type QuerySelector = string;
 type ClassName = string;
 type ContentSliderEvent = "open" | "close" | "caption_toggle" | "next" | "prev";
 
-interface ContentSliderOptions {
-    content: QuerySelector;
-    overlay: QuerySelector;
-    bemBlockName?: string;
-    swiperBemBlockName?: string;
-    extractSliderElement: (contentElement: Element) => Element;
-    extractCaption: (contentElement: Element) => string;
-    extractHashnavToken: (contentElement: Element, index: number) => string;
+declare class ContentSlider {
+    constructor(options:ContentSlider.ContentSliderOptions,
+                cssClasses?:ContentSlider.ContentSliderCSSClasses,
+                swiperOptions?:any);
+
+    openOverlay():void;
+
+    closeOverlay():void;
+
+    toggleCaption():void;
+
+    on(event:ContentSliderEvent, callback:(...args:any[]) => void);
 }
 
-interface ContentSliderCSSClasses {
-    overlay?: ClassName;
-    overlayModVisible?: ClassName;
-    nav?: ClassName;
-    navPosition?: ClassName;
-    caption?: ClassName;
-    captionModVisible?: ClassName;
-    elementContainer?: ClassName;
-    wrapper?: ClassName;
-    element?: ClassName;
-    icon?: ClassName,
-    toggleCaptionIcon?: ClassName,
-    toggleCaptionIconModActive?: ClassName,
-    prevIcon?: ClassName,
-    nextIcon?: ClassName,
-    closeIcon?: ClassName,
-}
+declare namespace ContentSlider {
+    export interface ContentSliderOptions {
+        content:QuerySelector;
+        overlay:QuerySelector;
+        bemBlockName?:string;
+        swiperBemBlockName?:string;
+        extractSliderElement:(contentElement:Element) => Element;
+        extractCaption:(contentElement:Element) => string;
+        extractHashnavToken:(contentElement:Element, index:number) => string;
+    }
 
-declare module "ContentSliderModule" {
-    export default class ContentSlider {
-        constructor(
-            options: ContentSliderOptions,
-            cssClasses?: ContentSliderCSSClasses,
-            swiperOptions?: any
-        );
-        openOverlay(): void;
-        closeOverlay(): void;
-        toggleCaption(): void;
-        on(event: ContentSliderEvent, callback: (...args: any[]) => void);
-    };
+    export interface ContentSliderCSSClasses {
+        overlay?:ClassName;
+        overlayModVisible?:ClassName;
+        nav?:ClassName;
+        navPosition?:ClassName;
+        caption?:ClassName;
+        captionModVisible?:ClassName;
+        elementContainer?:ClassName;
+        wrapper?:ClassName;
+        element?:ClassName;
+        icon?:ClassName,
+        toggleCaptionIcon?:ClassName,
+        toggleCaptionIconModActive?:ClassName,
+        prevIcon?:ClassName,
+        nextIcon?:ClassName,
+        closeIcon?:ClassName,
+    }
+
 }
